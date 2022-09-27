77c9d46609624396db16e33746b6aab7b5dbc6e6
yxo5017 <yxo5017@icloud.com>
2022-09-27 11:21:39 +0900

    your comment

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -1,13 +1,18 @@
 #----------------------------------------------------------------------------#
 # Imports
 #----------------------------------------------------------------------------#
+import collections
+import collections.abc
+collections.Callable = collections.abc.Callable
 
+import config
 import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -18,10 +23,12 @@ from forms import *
 
 app = Flask(__name__)
 moment = Moment(app)
+
+# TODO: connect to a local postgresql database
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-# TODO: connect to a local postgresql database
+migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
